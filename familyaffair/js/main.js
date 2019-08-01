65e4cd2dcf663b440fad9e4d3d5d209bcd4a3794
aymazhar <aymazhar@gmail.com>
2019-07-31 20:07:04 -0700

    updated recipes section
    recipe section added

--- familyaffair/js/main.js
+++ familyaffair/js/main.js
@@ -23,3 +23,21 @@ window.addEventListener('scroll', function() {
       );
     }
   });
+
+
+
+    // Smooth Scrolling Recipes
+    $('a').on('click', function(event) {
+      if (this.hash !== '') {
+        event.preventDefault();
+    
+        const hash = this.hash;
+    
+        $('html, body').animate(
+          {
+            scrollTop: $(hash).offset().top - 100
+          },
+          700
+        );
+      }
+    });
